110571d6a40565851f4c2f3244d897a62f9c6e6b
w_cixiangtao <w_cixiangtao@xiwang.com>
2024-01-04 18:22:17 +0800

    feat: ✨ 增加输出内容

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 import mdx from "@mdx-js/rollup";
+import unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 13579,
+  },
   plugins: [
     { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
     react(),
+    unocss(),
   ],
   resolve: {
     alias: {
